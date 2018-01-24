e41942eddbe054c1b73ae7675c80200c6ff61087
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2018-01-24 17:08:16 -0600

    Add about page and adjust eslint rules

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 const Index = () => (
   <div>
+    <nav>
+      <Link href='/about'>
+        <a>about</a>
+      </Link>
+    </nav>
     <h1>TEAM MOCKSY</h1>
   </div>
 );

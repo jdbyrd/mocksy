49b5b7207215af489c0b4e24b916df1bcf6625e3
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2018-01-24 18:25:06 -0600

    Add profile page and fix eslint

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,12 @@ import Link from 'next/link';
 const Index = () => (
   <div>
     <nav>
-      <Link href='/about'>
+      <Link href="/about">
         <a>about</a>
       </Link>
     </nav>
     <h1>TEAM MOCKSY</h1>
+    <p>Feed</p>
   </div>
 );
 

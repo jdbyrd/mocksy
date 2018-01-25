fed29885ca4bc586fdea17c669ae7c762cfabc44
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2018-01-25 11:05:38 -0600

    Remove nextJS pieces and add some files

---
+++ .next/bundles/pages/_document.js
@@ -0,0 +1,18 @@
+
+          window.__NEXT_REGISTER_PAGE('/_document', function() {
+            var comp = module.exports =
+webpackJsonp([1],{
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackMissingModule() { throw new Error("Cannot find module \"/Users/spencervaterlaus/Development/github.com/mocksy/mocksy/node_modules/next/dist/pages/_document.js?entry\""); }());
+
+
+/***/ })
+
+},[2]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlcy9wYWdlcy9fZG9jdW1lbnQuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0EiLCJzb3VyY2VSb290IjoiIn0=
+            return { page: comp.default }
+          })
+        
--- pages/about.js
+++
@@ -1,9 +0,0 @@
-const About = () => (
-  <div>
-    <p>
-      We here at Mocksy are swimming in moxie.
-    </p>
-  </div>
-);
-
-export default About;

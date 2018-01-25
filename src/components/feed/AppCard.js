709f418e67946d87d90a6ea368d0d6f6087bde8f
jackielfu <jackielfu@gmail.com>
2018-01-25 17:24:17 -0600

    Compartmentalize Styled Components

--- src/components/feed/AppCard.js
+++ src/components/feed/AppCard.js
@@ -1,20 +1,6 @@
 /* Contains screenshot of deployed app, title, author, description, tech stack, buttons to provide feedback or view feedback. */
 import React from 'react';
-
-// Styled Components buttons
-const Button = styled.button`
-  border-radius: 20px;
-  padding: 0.15em 0.75em;
-  margin: 0 1em;
-  background: white;
-  color: #323f48;
-  border: 1.5px solid #323f48;
-  
-  ${props => props.primary && css`
-    background: #323f48;
-    color: white;
-  `}
-`;
+import Button from '../shared.button.js';
 
 render(
   <div>

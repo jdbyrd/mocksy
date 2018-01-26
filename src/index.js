8a82cd653ddbec00f4101c85080c611c626d6383
Edward White <ewhite91186@gmail.com>
2018-01-26 12:18:34 -0600

    merge

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import FeedPage from './components/feed/FeedPage';
+import FeedbackPage from './components/feedback/FeedbackPage';
+import ProfilePage from './components/profile/ProfilePage';
+import Login from './components/login/Login';
 import Navbar from './components/shared/Navbar';
-
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+      <Navbar />
+        <Switch>
+          <Route exact path="/" render={() => (<FeedPage />)} />
+          <Route exact path="/project/:id" render={() => (<FeedbackPage />)} />
+          <Route exact path="/user/:name" render={() => (<ProfilePage />)} />
+          <Route exact path="/login" render={() => (<Login />)} />
+        </Switch>
       </div>
     );
   }
@@ -19,7 +28,9 @@ class App extends React.Component {
 ReactDOM.render(
   (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   ), document.getElementById('app')
 );

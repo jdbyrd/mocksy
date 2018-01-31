bb3576251a525c3dc330a3e3224ff7d17f7bdd34
Edward White <ewhite91186@gmail.com>
2018-01-31 15:52:57 -0600

    feed, profile, and navbar are responsive

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -10,9 +10,11 @@ class Navbar extends React.Component {
     this.state = {
       profilePic: 'http://2.bp.blogspot.com/-RJe3UG5Py1o/TzoOyLOMksI/AAAAAAAAA2U/metNEzpJnY8/s1600/funny-cat-face+1.jpg',
       viewMenu: false,
-      search: false
+      search: false,
+      menu: false
     };
     this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleMenu =this.toggleMenu.bind(this);
     this.toggleSearch = this.toggleSearch.bind(this);
   }
 
@@ -30,6 +32,12 @@ class Navbar extends React.Component {
     triangle.style.transform = 'perspective(500px) translate3d(153px, 0px, 0px)';
   }
 
+  toggleMenu() {
+    this.setState({
+      menu: !this.state.menu,
+    });
+  }
+
   toggleSearch() {
     console.log('search running')
     this.setState({ search: !this.state.search });
@@ -38,48 +46,60 @@ class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <NavContainer className="nav">
-          <Nav>
-            <Ul className="nav-links">
+        <div className="nav-wrapper">
+          <div className="nav-inner-wrapper">
+            <img className="hamburger-icon" onClick={this.toggleMenu} alt="hamburger-menu-icon" src="https://cdn0.iconfinder.com/data/icons/social-messaging-productivity-4/128/menu-2-512.png" />
+            <img className="bell-icon mobile-view-bell-icon" alt="bell-icon" src="https://www.materialui.co/materialIcons/social/notifications_grey_192x192.png" />
+            {this.state.menu &&
+            <div className="menu-dropdown-container">
+              <ul>
+                <li>Feed</li>
+                <li>Popular</li>
+                <li>Search</li>
+                <li>Login</li>
+              </ul>
+            </div>
+            }
+            <ul className="nav-links">
               <Link to='/'>
-                <Li onClick={this.triangleLeft}>Feed</Li>
+                <li onClick={this.triangleLeft}>Feed</li>
               </Link>
-              <Li onClick={this.triangleRight}>Popular</Li>
-              <Li>
+              <li onClick={this.triangleRight}>Popular</li>
+              <li>
                 <a href="/auth/github">Login</a>
-              </Li>
-            </Ul>
-            <RightContainer>
+              </li>
+            </ul>
+            <div className="right-container">
               <Button shape="circle" icon="search" onClick={this.toggleSearch} />
-              <Helper className="helper" />
-              <Bell className="bell-icon" src="https://www.materialui.co/materialIcons/social/notifications_grey_192x192.png" />
-              <ImgContainer className="user-img-container">
-                <ProfilePic
-                  src={this.state.profilePic}
-                  onClick={this.toggleDropdown}
-                />
-              </ImgContainer>
-            </RightContainer>
-          </Nav>
-          <Triangle id="triangle" />
-          {this.state.search ? <h4 className="searchResults">HAHA YOU CAN'T SEARCH!</h4> : null}
+              <span className="helper" />
+              <img className="bell-icon" src="https://www.materialui.co/materialIcons/social/notifications_grey_192x192.png" />
+              <img
+                className="profile-pic"
+                alt="profile-pic"
+                src={this.state.profilePic}
+                onClick={this.toggleDropdown}
+              />
+            </div>
+          </div>
+          <div id="triangle" />
+          {this.state.search ? <h4 className="searchResults">HAHA YOU CAN&#39;T SEARCH!</h4> : null}
           {this.state.viewMenu ?
-            <DropdownContainer>
-              <DropdownTriangle />
-              <Dropdown>
-                <DropdownUL>
-                  <List>Profile</List>
-                  <List>Your apps</List>
-                  <List>Your reviews</List>
-                  <List>Settings</List>
-                  <List>
+            <div className="dropdown-container">
+              <div className="dropdown-triangle" />
+              <div className="dropdown">
+                <ul>
+                  <li>Profile</li>
+                  <li>Your apps</li>
+                  <li>Your reviews</li>
+                  <li>Settings</li>
+                  <li>
                     <a href="/logout">Logout</a>
-                  </List>
-                </DropdownUL>
-              </Dropdown>
-            </DropdownContainer>
+                  </li>
+                </ul>
+              </div>
+            </div>
           : null}
-        </NavContainer>
+        </div>
       </div>
     );
   }
@@ -87,139 +107,43 @@ class Navbar extends React.Component {
 
 export default Navbar;
 
-const NavContainer = styled.div`
-  margin-bottom: 70px;
-`;
+// Don't delete these yet
+// const Search = styled.input`
+//   display: inline-block;
+//   padding-right: 10px;
+//   outline: none;
+//   width: 5px;
+//   height: 20px;
+//   border-radius: 15px;
+//   padding-left: 10px;
+//   -webkit-appearance: textfield;
+//   -webkit-box-sizing: content-box;
+//   font-family: inherit;
+//   font-size: 100%;
+//   color: transparent;
+//   cursor: pointer;
+//   background: #ededed url(https://static.tumblr.com/ftv85bp/MIXmud4tx/search-icon.png) no-repeat 4px center;
+//   -webkit-transition: all .5s;
+//   -moz-transition: all .5s;
+//   transition: all .5s;
+//   &:focus {
+//     width: 130px;
+//     color: #000;
+//     cursor: auto;
+//     padding-left: 27px;
+//     border-color: white;
+//   }
+// `;
 
-const Nav = styled.div`
-  width: 100%;
-  height 60px;
-  background: #90aab7;
-  display: grid;
-  grid-template-columns: 72% auto 27%;
-`;
+// const ImgContainer = styled.div`
+//   background: white;
+//   width: 40px;
+//   height: 40px;
+//   border-radius: 40px;
+//   display: inline-block;
+//   vertical-align: middle;
+//   border: 2px solid white;
+//   cursor: pointer;
+// `;
 
-const Ul = styled.ul`
-  padding: 0;
-  margin: 0;
-  list-style-type: none;
-  grid-column-start: 1;
-  grid-column-end: 2;
-  vertical-align: middle;
-`;
 
-const Li = styled.li`
-  display: inline-block;
-  padding: 20px 50px;
-  font-size: 20px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const RightContainer = styled.div`
-  grid-column-start: 3;
-  grid-column-end: 4;
-  text-align: right;
-  padding-right: 30px;
-`;
-
-const Search = styled.input`
-  display: inline-block;
-  padding-right: 10px;
-  outline: none;
-  width: 5px;
-  height: 20px;
-  border-radius: 15px;
-  padding-left: 10px;
-  -webkit-appearance: textfield;
-  -webkit-box-sizing: content-box;
-  font-family: inherit;
-  font-size: 100%;
-  color: transparent;
-  cursor: pointer;
-  background: #ededed url(https://static.tumblr.com/ftv85bp/MIXmud4tx/search-icon.png) no-repeat 4px center;
-  -webkit-transition: all .5s;
-  -moz-transition: all .5s;
-  transition: all .5s;
-  &:focus {
-    width: 130px;
-    color: #000;
-    cursor: auto;
-    padding-left: 27px;
-    border-color: white;
-  }
-`;
-
-const Helper = styled.span`
-  display: inline-block;
-  vertical-align: middle;
-  height: 100%;
-`;
-
-const Bell = styled.img`
-  height: 35px;
-  vertical-align: middle;
-  padding: 0 15px;
-  cursor: pointer;
-`;
-
-const ImgContainer = styled.div`
-  background: white;
-  width: 40px;
-  height: 40px;
-  border-radius: 40px;
-  display: inline-block;
-  vertical-align: middle;
-  border: 2px solid white;
-  cursor: pointer;
-`;
-
-const ProfilePic = styled.img`
-  width: 40px;
-  border-radius: 40px;
-`;
-
-const Triangle = styled.div`
-  border-left: 15px solid transparent;
-  border-right: 15px solid transparent;
-  border-top: 15px solid #90aab7;
-  margin-left: 57px;
-  width: 0;
-  height: 0;
-  transition-property: transform -webkit-transformv color;
-  transition-duration: 0.2s;
-  transition-timing-function: ease-in-out;
-`;
-
-const DropdownContainer = styled.div`
-  position: fixed;
-  right: 20px;
-  margin-top: -15px;
-`;
-
-const Dropdown = styled.div`
-  background: #90aab7;
-`;
-
-const DropdownTriangle = styled.div`
-  border-left: 15px solid transparent;
-  border-right: 15px solid transparent;
-  border-bottom: 15px solid #90aab7;
-  width: 0;
-  height: 0;
-  margin-left: 83px;
-`;
-
-const DropdownUL = styled.ul`
-  margin: 0;
-  list-style-type: none;
-  padding: 10px 20px;
-`;
-
-const List = styled.li`
-  padding: 5px 0;
-  &:hover {
-    cursor: pointer;
-  }
-`;

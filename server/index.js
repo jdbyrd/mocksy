54478fe64499a9aad664a2a92251377ca4677ffd
James Byrd <jd3yrd@gmail.com>
2018-01-30 21:25:15 -0600

    get requests for profile page works, need to test and assign aliases for full function

--- server/index.js
+++ server/index.js
@@ -110,7 +110,15 @@ app.get('/api/users', (req, res) => {
 
 app.get('/api/profile', (req, res) => {
   const { name } = req.query;
-  query.userFeedback(name).then(projects => console.log(projects));
+  query.users('jdbyrd').then(user => console.log(user));
+  query.userFeedback(name).then((projects) => {
+    const profile = { projects };
+    query.userFeedback(name).then((feedback) => {
+      profile.feedback = feedback;
+      console.log(profile);
+      res.send(profile);
+    });
+  });
 });
 
 app.post('/api/project', (req, res) => {

cc5542f1c906a28225e816365bf41c3ef32096b7
James Byrd <jd3yrd@gmail.com>
2018-02-09 12:27:11 -0600

    add react-motion for router animations

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { spring, AnimatedSwitch } from 'react-router-transition';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -14,6 +15,38 @@ import Navbar from './components/shared/Navbar';
 import SettingsPage from './components/settings/SettingsPage';
 import { checkAuth, populateFeed } from './actions/index';
 import './app.scss';
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
 
 class App extends React.Component {
   constructor() {
@@ -46,14 +79,20 @@ class App extends React.Component {
     return (
       <div>
         <Navbar changeTriangle={this.changeTriangle} homepage={this.state.homepage} />
-        <Switch>
+        <AnimatedSwitch
+          atEnter={bounceTransition.atEnter}
+          atLeave={bounceTransition.atLeave}
+          atActive={bounceTransition.atActive}
+          mapStyles={mapStyles}
+          className="route-wrapper"
+        >
           <Route exact={true} path="/" render={() => (<FeedPage feed={this.state.triangle} isHomepage={this.isHomepage} />)} />
           <Route path="/project/:id" render={(props) => (<FeedbackPage {...props} isHomepage={this.isHomepage} />)} />
           <Route path="/user/:name" render={(props) => (<ProfilePage {...props} isHomepage={this.isHomepage} />)} />
           <Route path="/settings/:name" render={() => (<SettingsPage isHomepage={this.isHomepage} />)} />
           <Route path="/login" render={() => (<Login isHomepage={this.isHomepage} />)} />
           <Route render={() => (<div>404 lol</div>)} />
-        </Switch>
+        </AnimatedSwitch>
       </div>
     );
   }

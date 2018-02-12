3c1df6a180638e73ed157dca4e865841e377fa88
Edward White <ewhite91186@gmail.com>
2018-02-11 18:52:44 -0600

    bars transition up from the bottom, made graph more readable by shortening names

--- src/components/feedback/AppSidebar.js
+++ src/components/feedback/AppSidebar.js
@@ -4,13 +4,21 @@ import Chart from './Chart';
 
 const mapStateToProps = state => (
   {
-    project: state.feedback.project
+    project: state.feedback.project,
+    feedbackItems: state.feedback.list
   }
 );
 
 class AppSidebar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      feedbackItems: []
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ feedbackItems: nextProps.feedbackItems });
   }
 
   render() {
@@ -32,9 +40,13 @@ class AppSidebar extends React.Component {
         <p>
           {project.text}
         </p>
-        <svg width="300" height="300" className="svg">
-          <Chart width={300} height={300} clickGraph={this.props.clickGraph} />
+        {this.state.feedbackItems.length ?
+          <h3 className="chart-title">Feedback by type</h3>
+        : null}
+        <svg width="400" height="250" className="svg">
+          <Chart width={400} height={250} clickGraph={this.props.clickGraph} />
         </svg>
+
       </div>
     );
   }

761a85ee2b867de432e734eb0f403cabcc74aa48
James Byrd <jd3yrd@gmail.com>
2018-02-15 17:14:04 -0600

    add contributers to deletes

--- database/deletes.js
+++ database/deletes.js
@@ -4,6 +4,8 @@ const project = id => knex('projects').where({ id }).del();
 
 const tags = project_id => knex('tags').where({ project_id }).del();
 
+const contributers = project_id => knex('contributers').where({ project_id }).del();
+
 const projectFeedback = id => knex('feedback').where({ project_id: id }).del();
 
 const feedback = id => knex('feedback').where({ id }).del();
